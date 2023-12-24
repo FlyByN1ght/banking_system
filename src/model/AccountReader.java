bb83e78d5e55462716306c94ae83e3aef46bf3ee
FlyByN1ght <savitskiy.daniil@gmail.com>
2023-12-24 15:30:24 +0300

    Added method updateAccountsFile

--- src/model/AccountReader.java
+++ src/model/AccountReader.java
@@ -1,13 +1,27 @@
+package model;
+
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
+/**
+ *
+ * @author Даниил
+ * verion 1.1
+ */
 public class AccountReader {
 
     private static final String ACCOUNTS_FILE = "C:\\Users\\Daniil\\IdeaProjects\\banking_system\\src\\accounts.txt";
 
+    /**
+     * Читает аккаунты из файла и возвращает их в виде Map.
+     *
+     * @return Map, где ключ - номер счета, значение - баланс
+     */
     public static Map<String, Integer> readAccounts() {
         Map<String, Integer> accounts = new HashMap<>();
 
@@ -26,5 +40,20 @@ public class AccountReader {
             System.err.println("Файл не найден: " + ACCOUNTS_FILE);
         }
         return accounts;
+    }
+
+    /**
+     * Обновляет файл с аккаунтами на основе переданной Map.
+     *
+     * @param accounts Map с аккаунтами для записи в файл
+     */
+    public static void updateAccountsFile(Map<String, Integer> accounts) {
+        try (FileWriter writer = new FileWriter(AccountReader.ACCOUNTS_FILE)) {
+            for (Map.Entry<String, Integer> entry : accounts.entrySet()) {
+                writer.write(entry.getKey() + ":" + entry.getValue() + "\n");
+            }
+        } catch (IOException e) {
+            System.err.println("Ошибка при записи в файл accounts.txt: " + e.getMessage());
+        }
     }
 }
